afa93cdfcffc139c77c6440e885e6d1b886681d3
Alexander Marin <maal18en@student.ju.se>
2019-03-21 11:04:28 +0100

    Add project files.

---
+++ JourneyEntityFrameworkVersion/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JourneyEntityFrameworkVersion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
